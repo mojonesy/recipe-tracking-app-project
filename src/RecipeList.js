713431318f770a08095e45eb3d7c0b5a8f97c4e6
Molly <mojonesycreative@gmail.com>
2022-04-20 18:03:33 -0400

    Labeled table headers

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Recipe from "./Recipe";
 
-function RecipeList() {
+function RecipeList({ recipes, deleteRecipe }) {
   
   // TODO: Display the list of recipes using the structure of table that is provided.
   // TODO: Create at least one additional component that is used by this component.
@@ -12,7 +12,11 @@ function RecipeList() {
       <table>
         <thead>
           <tr>
-            <th></th>
+            <th>Name</th>
+            <th>Cuisine</th>
+            <th>Photo</th>
+            <th>Ingredients</th>
+            <th>Preparation</th>
             <th>Actions</th>
           </tr>
         </thead>
