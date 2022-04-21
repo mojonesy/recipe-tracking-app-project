3e7feb2b25b42941af73e0aae8148f3c4a1e09d3
Molly Jones <84247355+mojonesy@users.noreply.github.com>
2022-04-21 17:52:57 -0400

    added fragments

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,16 +2,18 @@ import React from "react";
 
 function Recipe({ recipe, deleteRecipe }) {
     return (
-        <tr>
-            <td className="content"><p>{(recipe.name)}</p></td>
-            <td className="content"><p>{(recipe.cuisine)}</p></td>
-            <td className="content"><img src ={(recipe.photo)} /></td>
-            <td className="content_td"><p>{(recipe.ingredients)}</p></td>
-            <td className="content_td"><p>{(recipe.preparation)}</p></td>
-            <td className="content">
-                <button name="delete" onClick={deleteRecipe}>Delete</button>
-            </td>
-        </tr>
+        <>
+            <tr>
+                <td className="content"><p>{recipe.name}</p></td>
+                <td className="content"><p>{recipe.cuisine}</p></td>
+                <td className="content"><img src ={recipe.photo} /></td>
+                <td className="content_td"><p>{recipe.ingredients}</p></td>
+                <td className="content_td"><p>{recipe.preparation}</p></td>
+                <td className="content">
+                    <button name="delete" onClick={deleteRecipe}>Delete</button>
+                </td>
+            </tr>
+        </>
     )
 }
 
